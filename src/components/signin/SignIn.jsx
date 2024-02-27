c1c00364dc2911bf8eacca32c764e33a36af9bff
sanathsb98 <itssanathsb@gmail.com>
2024-02-27 11:38:25 +0530

    commit

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -1,104 +1,71 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../signin/SignIn.css';
 import { useDispatch } from 'react-redux';
 import { registerState } from '../../features/registerSlice';
-import bcrypt from 'bcryptjs';
 import loadingIcon from '/src/images/loadingicon.gif';
 
 const SignIn = () => {
 
-  const[isAuthenticated,setIsAuthenticated] = useState()
-  const[isWrongPassword,setIsWrongPassword] = useState()
-  const[isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [loginStatus, setLoginStatus] = useState({ message: '' })
 
   var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const[loginInfo,setLoginInfo] = useState({
-    email : '',
-    password : '',
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
+    password: '',
   })
 
-  useEffect(()=>{
-  setIsLoading(false)
-  },[])
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
 
   const isValidEmail = emailPattern.test(loginInfo.email)
 
   const getLoginInfo = (event) => {
-    const {name,value} = event.target;
-    setLoginInfo((prev)=>({...prev,[name]:value}))
+    const { name, value } = event.target;
+    setLoginInfo((prev) => ({ ...prev, [name]: value }))
     console.log(loginInfo)
   }
 
-  const isLoginFieldEmpty = Object.values(loginInfo).every((value)=>value != '')
+  const isLoginFieldEmpty = Object.values(loginInfo).every((value) => value != '')
 
   const userLogin = async () => {
 
-    console.log(isAuthenticated)
-    console.log(isWrongPassword)
+    const data = {
+      email: loginInfo.email,
+      password: loginInfo.password
+    }
 
-    try{
+    try {
       setIsLoading(true)
-      const response = await fetch(`https://ecommerce-backend-eight-azure.vercel.app/api/signin?email=${loginInfo.email}`,{
-        method : 'GET',
-        headers : {
-          'Content-Type' : 'application/json',
+      const response = await fetch("https://ecommerce-backend-eight-azure.vercel.app/api/signin", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      
-       })
-       
-       if(!response){
-        setIsAuthenticated(false)
-        setIsLoading(false)
-        throw new Error('cant get login details')
-       
-       }
-    
-       // getting and setting the logged user deatils in a state:
-     
-       const {loginDetails} = await response.json()
-       if(loginDetails === null){
-        setIsAuthenticated(false)
-       }else{
-        setIsAuthenticated(true)
-       }
-
-       console.log("login details:",loginDetails)
-      
-     
-
-      // checking if the hashed password and actual password is same:
-      const hashedPassword = loginDetails.password;
-      await bcrypt.compare(loginInfo.password, hashedPassword, (err, result) => {
-        if (err) {
-          console.log('error')
-        }
-        if (result) {
-
-          console.log('password matched')
-          window.location.href = "/home";
-        
-          setIsWrongPassword(false)
-          setIsLoading(false)
-
-        } else {
-
-          console.log('wrong password')
-          
-          setIsWrongPassword(true)
-          setIsLoading(false)
-        }
+        body: JSON.stringify(data)
       })
+
+      if (!response) {
+     
+        throw new Error('cant get login details')
+        setIsLoading(false)
+      }
+
+      // getting and setting the logged user deatils in a state:
+
+      const loginDetails = await response.json()
+      setLoginStatus(loginDetails)
+      console.log("login details:", loginDetails)
       setIsLoading(false)
+
     }
     catch (err) {
       console.log(err)
       setIsWrongPassword(true)
       setIsLoading(false)
-   
-      
     }
-    setIsLoading(false)
   }
 
   const dispatch = useDispatch()
@@ -111,56 +78,38 @@ const SignIn = () => {
   return (
     <div className='signup-container'>
 
-    <div className='signup-title-des'>
-      <div className='signup-title font-title'>Welcome👋 </div>
-      <div className='signup-description'>Please login here</div>
-    </div>
-
-    <div className='signup-auth-boxes'>
-
-      
-
-      <div className='auth-input-box'>
-        <div className='auth-input-title'>Email Address</div>
-        <input name='email' value={loginInfo.email} onChange={(e)=>{getLoginInfo(e)}} className='signup-input-field' type='text' />
+      <div className='signup-title-des'>
+        <div className='signup-title font-title'>Welcome👋 </div>
+        <div className='signup-description'>Please login here</div>
       </div>
 
-    
+      <div className='signup-auth-boxes'>
+        <div className='auth-input-box'>
+          <div className='auth-input-title'>Email Address</div>
+          <input name='email' value={loginInfo.email} onChange={(e) => { getLoginInfo(e) }} className='signup-input-field' type='text' />
+        </div>
 
-      <div className='auth-input-box'>
-        <div className='auth-input-title'>Password</div>
-        <input name='password' value={loginInfo.password} onChange={(e)=>{getLoginInfo(e)}} className='signup-input-field' type='text' />
-
-       {isAuthenticated === false &&  isWrongPassword === true ? (  <div className='auth-input-error'>user not registered</div>) 
-       :isAuthenticated === true &&  isWrongPassword === true ?
-       ( <div className='auth-input-error'>password is wrong</div>)
-       : 
-       (<></>)
-       
-       }
-      
+        <div className='auth-input-box'>
+          <div className='auth-input-title'>Password</div>
+          <input name='password' value={loginInfo.password} onChange={(e) => { getLoginInfo(e) }} className='signup-input-field' type='text' />
+          <div className='auth-input-error'>{loginStatus.message}</div>
+        </div>
 
       </div>
 
-    </div>
+      <div className='signup-auth-checkbox'>
+        <input type='checkbox' id='agreetocheckbox' />
+        <label for="agreetocheckbox">Remember Me</label>
+      </div>
 
-    
-
-    <div className='signup-auth-checkbox'>
-      <input type='checkbox' id='agreetocheckbox' />
-      <label for="agreetocheckbox">Remember Me</label>
-    </div>
-
-    <button disabled={!isValidEmail} onClick={()=>{userLogin()}} style={{opacity:`${isLoginFieldEmpty && isValidEmail ? '1' : '0.3'}`}} className={`${isLoginFieldEmpty && isValidEmail ? 'signup-auth-button cursor' : 'signup-auth-button'}`} >Login</button>
+      <button disabled={!isValidEmail} onClick={() => { userLogin() }} style={{ opacity: `${isLoginFieldEmpty && isValidEmail ? '1' : '0.3'}` }} className={`${isLoginFieldEmpty && isValidEmail ? 'signup-auth-button cursor' : 'signup-auth-button'}`} >Login</button>
 
       {isLoading ? (<div className='loading-icon-box'>
         <img style={{ width: '30px', height: '30px' }} src={loadingIcon} />
       </div>) : ("")}
 
-
-  
-    <div>not registered ? <b onClick={()=>changeLoginStatus('notregistered')} className='terms-conditions'>register</b> </div>
-  </div>
+      <div>not registered ? <b onClick={() => changeLoginStatus('notregistered')} className='terms-conditions'>register</b> </div>
+    </div>
   )
 }
 
